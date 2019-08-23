b04fd1d858b6e9fd14d94e570a4bf1ffa86ba71e
Rozi Tunyan <SleepyR>
2019-08-23 17:52:08 +0400

    after login redirect /companies

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/companies');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/companies', 'HomeController@index')->name('home');
